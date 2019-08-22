0f99bfba8a8e42493c128a2111bd61fcf7823ae2
ollie <leyechi@gmail.com>
2019-08-21 22:06:21 -0700

    dynamically rendering header

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import renderImage from './render-image.js';
 import htmlToDOM from './html-to-DOM.js';
 import images from '../data/images.js';
+import renderHeader from './render-header.js';
 
+const header = document.querySelector('.header');
 const list = document.querySelector('.photo-gallery');
+
+addHeader();
 
 images.forEach(photo => {
     const html = renderImage(photo);
     const dom = htmlToDOM(html);
     list.appendChild(dom);
 });
+
+function addHeader() {
+    const html = renderHeader();
+    const dom = htmlToDOM(html);
+    header.appendChild(dom);
+}
